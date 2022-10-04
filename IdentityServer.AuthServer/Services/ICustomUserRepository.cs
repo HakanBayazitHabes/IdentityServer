942bce1f9b730d3138ef9731f16d8aeb78909c92
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2022-10-05 01:16:56 +0300

    add ICustomUserRespository

---
+++ IdentityServer.AuthServer/Services/ICustomUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using IdentityServer.AuthServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.AuthServer.Services
+{
+    public interface ICustomUserRepository
+    {
+        Task<bool> Validate(string email, string password);
+        Task<CustomUser> FindById(int id);
+        Task<CustomUser> FindByEmail(string email);
+    }
+}
